a7a11dc96cfd822dd14441a2459da04e5f2f8de3
SudoSavvy <donovanjheape@gmail.com>
2024-08-28 18:40:16 -0500

    Coding in vscode with python

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str)
+print(str.split()[8], str.split()[9], str.split()[10], str.split()[11], str.split()[12])
