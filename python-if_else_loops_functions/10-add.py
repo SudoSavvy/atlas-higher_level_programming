9c1bf56edf66b3a4f0f2019c15610575dbbc2ad2
SudoSavvy <donovanjheape@gmail.com>
2024-08-29 21:15:07 -0500

    Coding in vscode with python day 2

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-def add(a, b):
-    return a + b
 
-# Example usage
-a = 5
-b = 13
-result = add(a, b)
-print(result)
+def add(a, b):
+    try:
+        return a + b
+    except TypeError:
+        return None
+
+print(add(100, -2))
+print(add(-100, -2))
+print(add(0, 0))
+print(add(98, "Holberton"))
+print(add(1, 2))
