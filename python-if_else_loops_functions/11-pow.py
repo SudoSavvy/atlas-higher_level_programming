e7e53cacef7e2891c3613f178866b3b20d40a0c4
SudoSavvy <donovanjheape@gmail.com>
2024-08-29 22:00:43 -0500

    Coding in vscode with python day 2

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -6,6 +6,4 @@ def pow(a, b):
     result = 1.0
     for _ in range(b):
         result *= a
-    if result == int(result):
-        return int(result)
-    return result
+    return round(result, 2)
