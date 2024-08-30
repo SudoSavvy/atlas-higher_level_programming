2e78e83fcaae616d75a9da9a48eac465d17cb1c0
SudoSavvy <donovanjheape@gmail.com>
2024-08-30 10:57:35 -0500

    Coding in python day 3

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,15 +2,15 @@
 import sys
 
 if __name__ == "__main__":
-    argv = sys.argv[1:]  # Exclude the script name
-    argc = len(argv)  # Get the number of arguments
+    argv = sys.argv[1:]
+    argc = len(argv)
 
     if argc == 0:
         print("Number of argument(s): 0.")
     elif argc == 1:
-        print("Number of argument(s): 1 argument:")
+        print("1 argument:")
         print("1: {}".format(argv[0]))
     else:
-        print("Number of argument(s): {} arguments:".format(argc))
+        print("{} arguments:".format(argc))
         for i in range(argc):
             print("{}: {}".format(i + 1, argv[i]))
