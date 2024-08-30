e635b8ab86b492e962abad3f7827a8afc23825fc
SudoSavvy <donovanjheape@gmail.com>
2024-08-30 11:04:02 -0500

    Coding in python day 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,10 +3,10 @@ import sys
 
 if __name__ == "__main__":
     args = sys.argv[1:]
-    
+
     total_sum = 0
-    
+
     for arg in args:
         total_sum += int(arg)
-    
+
     print(total_sum)
