fee69fbee1927c4bab0bf9fd0c48476de277582e
SudoSavvy <donovanjheape@gmail.com>
2024-08-29 22:04:21 -0500

    Coding in vscode with python day 2

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -3,8 +3,8 @@ def pow(a, b):
     if b < 0:
         a = 1 / a
         b = -b
-    result = 1
+    result = 1.0
     for _ in range(b):
         result *= a
-    # Format result to ensure correct precision
+    # Ensure result is formatted with appropriate precision
     return "{:.10g}".format(result)
