5e32a0c0439453cfe25808051d6458bbde6580c6
SudoSavvy <donovanjheape@gmail.com>
2024-08-29 20:18:57 -0500

    Coding in vscode with python day 2

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
+    result = ""
     for char in str:
-        ascii_value = ord(char)
-        if 97 <= ascii_value <= 122:
-            print(chr(ascii_value - 32), end="")
+        if 'a' <= char <= 'z':
+            result += chr(ord(char) - 32)
         else:
-            print(char, end="")
-    print()
+            result += char
+    print(result)
