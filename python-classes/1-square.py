d1ad04a11c1bce384bd3e785cea1853e4dccce92
SudoSavvy <donovanjheape@gmail.com>
2024-09-05 14:49:42 -0500

    Day 1 of sprint 2. Classes & objects.

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This module defines a class Square.
+The Square class represents a square with a private size attribute.
+"""
+
 class Square:
     """
     A class that defines a square with a private size attribute.
@@ -8,5 +13,4 @@ class Square:
         """
         Initialize the square with a given size.
         """
-        self.__size = size #This is the private attribute.
-        
+        self.__size = size  # This is the private attribute.
