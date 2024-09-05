31b34e77b72631398dea913d07a42865dcaae28d
SudoSavvy <donovanjheape@gmail.com>
2024-09-05 14:07:52 -0500

    Day 1 of sprint 2. Classes & objects.

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class Square.
+The class currently does not have any attributes or methods.
+"""
+
+
 class Square:
+    """
+    An empty class that defines a square.
+    Currently, it has no properties or methods.
+    """
     pass
+
+
+# Test the Square class
+mysquare = Square()
+print(type(mysquare))         # Expected: <class '__main__.Square'>
+print(mysquare.__dict__)      # Expected: {}
