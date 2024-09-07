9ed6c2999b3c78ff0d2713ac771d746f90706b36
SudoSavvy <donovanjheape@gmail.com>
2024-09-07 13:49:41 -0500

    Day 2 of sprint 2. Test driven dev.

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""
+Module documentation for Rectangle.
+This module defines an empty Rectangle class.
+"""
+
+
 class Rectangle:
+    """An empty class that defines a rectangle."""
     pass
