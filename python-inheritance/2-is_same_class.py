387af0757053ae01329c813ef69a65bae4b43395
SudoSavvy <donovanjheape@gmail.com>
2024-09-14 22:48:21 -0500

    Day 3 of sprint 3. Inheritance.

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+Module to define a function that checks if an object is exactly an instance
+of a specified class.
+
+Function:
+- is_same_class: Returns True if obj is exactly an instance of a_class;
+  otherwise False.
+"""
+
 def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of a_class; otherwise False."""
+    """
+    Returns True if obj is exactly an instance of a_class; otherwise False.
+
+    Parameters:
+    obj: The object to check.
+    a_class: The class to compare against.
+
+    Returns:
+    bool: True if obj is exactly an instance of a_class, otherwise False.
+    """
     return type(obj) is a_class
