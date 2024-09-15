41b1a758a94cef67411d617a13d69d6a2bd7544d
SudoSavvy <donovanjheape@gmail.com>
2024-09-14 22:58:05 -0500

    Day 3 of sprint 3. Inheritance.

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
+"""
+6-base_geometry.py
+
+This module defines the BaseGeometry class.
+"""
 
 class BaseGeometry:
+    """
+    A base class for geometric shapes.
+
+    Methods:
+        area(self): Raises an Exception indicating that the method is not implemented.
+    """
     def area(self):
+        """
+        Method that raises an Exception with the message "area() is not implemented".
+
+        Raises:
+            Exception: with the message "area() is not implemented"
+        """
         raise Exception("area() is not implemented")
