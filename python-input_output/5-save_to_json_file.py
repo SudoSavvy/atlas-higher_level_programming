0010bc7dfdbd286ee102884835784b942971b276
SudoSavvy <donovanjheape@gmail.com>
2024-09-15 16:36:28 -0500

    Day 4 of sprint 3. Input/output.

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module for saving an object to a text file in JSON format."""
 
 def to_json_string(my_obj):
     """Convert a Python object to a JSON string representation.
@@ -8,6 +9,9 @@ def to_json_string(my_obj):
 
     Returns:
         str: The JSON string representation of the object.
+
+    Raises:
+        TypeError: If the object is not serializable to JSON.
     """
     if isinstance(my_obj, dict):
         items = [f'"{key}": {to_json_string(value)}' for key, value in my_obj.items()]
@@ -32,33 +36,9 @@ def save_to_json_file(my_obj, filename):
     Args:
         my_obj (object): The object to write to the file.
         filename (str): The name of the file to write to.
+
+    The function converts the Python object to a JSON string and writes it to the specified file.
     """
     json_string = to_json_string(my_obj)
     with open(filename, 'w') as file:
         file.write(json_string)
-
-# Sample usage
-if __name__ == "__main__":
-    filename = "my_list.json"
-    my_list = [1, 2, 3]
-    save_to_json_file(my_list, filename)
-
-    filename = "my_dict.json"
-    my_dict = { 
-        'id': 12,
-        'name': "John",
-        'places': ["San Francisco", "Tokyo"],
-        'is_active': True,
-        'info': {
-            'age': 36,
-            'average': 3.14
-        }
-    }
-    save_to_json_file(my_dict, filename)
-
-    try:
-        filename = "my_set.json"
-        my_set = {132, 3}
-        save_to_json_file(my_set, filename)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
