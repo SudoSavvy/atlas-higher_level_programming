d64160669076dc8b5e409b8af25cd4a118995515
SudoSavvy <donovanjheape@gmail.com>
2024-09-15 01:26:24 -0500

    Day 3 of sprint 3. Inheritance.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,22 +1,6 @@
 #!/usr/bin/python3
 """Module for Rectangle class inheriting from BaseGeometry."""
 
-from base_geometry import BaseGeometry
-
-class Rectangle(BaseGeometry):
-    """Rectangle class that inherits from BaseGeometry."""
-    
-    def __init__(self, width, height):
-        """Initialize the Rectangle with width and height."""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def __str__(self):
-        """Re#!/usr/bin/python3
-"""Module for Rectangle class inheriting from BaseGeometry."""
-
 class BaseGeometry:
     """BaseGeometry class with basic validation methods."""
 
@@ -26,7 +10,6 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-
 
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits from BaseGeometry."""
@@ -40,18 +23,6 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Return a string representation of the Rectangle."""
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-    @property
-    def width(self):
-        """Return the width of the Rectangle."""
-        return self.__width
-
-    @property
-    def height(self):
-        """Return the height of the Rectangle."""
-        return self.__height
-turn a string representation of the Rectangle."""
         return f"[Rectangle] {self.__width}/{self.__height}"
 
     @property
