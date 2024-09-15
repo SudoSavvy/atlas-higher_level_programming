f56fced83454e868adc05aba188a8c373fa0374a
SudoSavvy <donovanjheape@gmail.com>
2024-09-14 22:38:02 -0500

    Day 3 of sprint 3. Inheritance.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -10,3 +10,7 @@ class MyList(list):
     def print_sorted(self):
         """Prints the list elements in sorted (ascending) order."""
         print(sorted(self))
+    
+    def __str__(self):
+        """Return a string representation of the list."""
+        return super().__str__()
