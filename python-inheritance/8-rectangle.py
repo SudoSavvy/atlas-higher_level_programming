b9e8f701be1afa6eeb0abca682af432023e41770
SudoSavvy <donovanjheape@gmail.com>
2024-09-15 01:23:21 -0500

    Day 3 of sprint 3. Inheritance.

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module for Rectangle class inheriting from BaseGeometry."""
+
+from base_geometry import BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry."""
+    
+    def __init__(self, width, height):
+        """Initialize the Rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """Return a string representation of the Rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    @property
+    def width(self):
+        """Get the width of the Rectangle."""
+        return self.__width
+
+    @property
+    def height(self):
+        """Get the height of the Rectangle."""
+        return self.__height
