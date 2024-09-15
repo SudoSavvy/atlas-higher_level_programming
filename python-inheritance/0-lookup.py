d121367ccb840a5a59b43e8ee1618c2457bc0695
SudoSavvy <donovanjheape@gmail.com>
2024-09-14 22:22:18 -0500

    Day 3 of sprint 3. Inheritance.

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 This module provides a function to return the list of available attributes and methods of an object.
 """
 
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
