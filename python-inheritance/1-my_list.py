89ec5025c83b63fc061b45290d8717a2f9ad397c
SudoSavvy <donovanjheape@gmail.com>
2024-09-15 15:04:48 -0500

    Day 3 of sprint 3. Inheritance.

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-This module defines the MyList class, which inherits from list.
+This module defines the MyList class.
 """
 
 class MyList(list):
     """
     A subclass of list with an additional method to print the list in sorted order.
     """
-
     def print_sorted(self):
         """
         Prints the list in ascending order without modifying the original list.
