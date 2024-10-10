5e0c2b12e2258590eed5d066532c7c962184030a
SudoSavvy <donovanjheape@gmail.com>
2024-10-10 12:46:24 -0500

    MySQL project work

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,13 @@
 -- This script prints the full description of the first_table
 SELECT 
     TABLE_NAME AS 'Table',
-    CREATE_STATEMENT AS 'Create Table'
+    CONCAT('CREATE TABLE `', TABLE_NAME, '` (',
+           '`id` INT NOT NULL AUTO_INCREMENT, ',
+           '`name` VARCHAR(128) DEFAULT NULL, ',
+           '`c` CHAR(1) DEFAULT NULL, ',
+           '`created_at` DATE DEFAULT NULL, ',
+           'PRIMARY KEY (`id`)) ',
+           'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci') AS 'Create Table'
 FROM 
     information_schema.TABLES 
 WHERE 
