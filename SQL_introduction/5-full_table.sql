60e3ec7c14494b6025540de3d3fa39270c8dd28c
SudoSavvy <donovanjheape@gmail.com>
2024-10-10 12:58:14 -0500

    MySQL project work

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,17 +1,2 @@
--- This script prints the full description of the first_table
-SELECT 
-    TABLE_NAME AS 'Table',
-    CONCAT(
-        'CREATE TABLE `', TABLE_NAME, '` (',
-        '`id` INT NOT NULL AUTO_INCREMENT,',
-        '`name` VARCHAR(128) DEFAULT NULL,',
-        '`c` CHAR(1) DEFAULT NULL,',
-        '`created_at` DATE DEFAULT NULL,',
-        'PRIMARY KEY (`id`)',
-        ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
-    ) AS 'Create Table'
-FROM 
-    information_schema.TABLES 
-WHERE 
-    TABLE_SCHEMA = 'hbtn_0c_0' 
-    AND TABLE_NAME = 'first_table';
+-- Write a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server.
+SHOW CREATE TABLE first_table;
