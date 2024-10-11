14b46b60defa0108e8e69fa7eb51fe61a5c4e7b1
SudoSavvy <donovanjheape@gmail.com>
2024-10-11 13:18:22 -0500

    More MySQL project work

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
--- Select the title of the TV shows and their corresponding genre IDs
+-- Select the title of TV shows and their corresponding genre IDs
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows  -- From the tv_shows table
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id  -- Join with the tv_show_genres table on matching IDs
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;  -- Sort the results by show title and genre ID in ascending order
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id  -- Update with the correct column name
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;  -- Sort the results
