fcc92dbf16213a21b11feb3b32768ffb97f571ad
SudoSavvy <donovanjheape@gmail.com>
2024-10-11 13:12:12 -0500

    More MySQL project work

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- List all cities with their corresponding states
+SELECT cities.id, cities.name, 
+       (SELECT states.name FROM states WHERE states.id = cities.state_id) AS state_name
+FROM cities
+ORDER BY cities.id ASC;
