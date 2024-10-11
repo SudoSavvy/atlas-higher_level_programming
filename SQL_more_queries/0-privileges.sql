c1386b81c90cc3d79b620d890411e0ec3e37a971
SudoSavvy <donovanjheape@gmail.com>
2024-10-11 12:07:58 -0500

    More MySQL project work

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,17 +1,5 @@
--- Create user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- List all privileges for user_0d_1
+-- Attempt to show privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List all privileges for user_0d_2
+-- Attempt to show privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
