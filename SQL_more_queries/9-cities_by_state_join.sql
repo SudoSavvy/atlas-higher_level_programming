95214aa5be3b96dfe62b20f884d31daf51026fbf
SudoSavvy <donovanjheape@gmail.com>
2024-10-11 13:15:31 -0500

    More MySQL project work

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,4 @@
 -- List all cities with their corresponding states
-SELECT cities.id, cities.name, 
 SELECT cities.id, cities.name, states.name 
 FROM cities, states 
 WHERE cities.state_id = states.id 
