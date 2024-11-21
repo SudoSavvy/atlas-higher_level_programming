764b69dfbdf23bcc8e30ae3dcad55b239078e0b0
SudoSavvy <donovanjheape@gmail.com>
2024-11-21 15:36:39 -0600

    Javascript project work

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const fs = require('fs');
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Read the file in utf-8 encoding
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
