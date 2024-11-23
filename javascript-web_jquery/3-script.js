bf4da7f328f9e8536f3a9ed809e7d00e31560037
SudoSavvy <donovanjheape@gmail.com>
2024-11-23 01:05:12 -0600

    Javascript project work

---
+++ javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+// Add the class "red" to the <header> element when the user clicks the tag DIV#red_header
+$(document).ready(function () {
+    $('#red_header').click(function () {
+      $('header').addClass('red');
+    });
+  });
+  
