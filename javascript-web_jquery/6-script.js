305df5e70a714a23fa1d4068db1e9b9106a695a9
SudoSavvy <donovanjheape@gmail.com>
2024-11-23 01:07:00 -0600

    Javascript project work

---
+++ javascript-web_jquery/6-script.js
@@ -0,0 +1,7 @@
+// Updates the text of <header> to "New Header!!!" when DIV#update_header is clicked
+$(document).ready(function () {
+    $('#update_header').click(function () {
+      $('header').text('New Header!!!');
+    });
+  });
+  
